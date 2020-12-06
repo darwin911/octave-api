262896fb78e197dc1edb33e6d0f45bd60e7f5fe3
Darwin Smith <darwin911@gmail.com>
2020-12-06 17:39:59 -0500

    WIP: refactor for passport

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
+const session = require('express-session');
 const passport = require('passport');
+const cors = require('cors');
+let routes = require('./routes');
 
+// Gives us access to variables set in the .env file via `process.env.VARIABLE_NAME` syntax
 require('dotenv').config();
 
+// Create the Express application
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+require('./config/passport');
+
+/**
+ * -------------- PASSPORT AUTHENTICATION ----------------
+ */
+
 app.use(passport.initialize());
+app.use(passport.session());
+
+app.use((req, res, next) => {
+  console.log(req.session);
+  console.log(req.user);
+  next();
+});
+
+/**
+ * -------------- DATABASE CONNECTION ----------------
+ */
 
 const uri = process.env.MONGODB_URI;
 
@@ -26,14 +48,25 @@ connection.once('open', () => {
   console.log('*** MongoDB database connection established successfully ***');
 });
 
-const authRouter = require('./routes/auth');
+/**
+ * -------------- ROUTES ----------------
+ */
+
+// Imports all of the routes from ./routes/index.js
+app.use(routes);
+
+const authRouter = require('./routes');
 
 app.use('/auth', authRouter);
+
+/**
+ * -------------- SERVER ----------------
+ */
 
 app.get('/', (req, res) => {
   res.json({ message: `Server is running on port: ${port}` });
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
+  console.log(`Server is listening on port: ${port}`);
 });
