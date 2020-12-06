8a2bd5588194abad4194b16d4f751809dcb87821
Darwin Smith <darwin911@gmail.com>
2020-12-05 20:02:08 -0500

    Revert "boilerplate register/login route"
    This reverts commit 3bd27ca42639c12c6a82481167483177956af010.

--- routes/auth.js
+++ routes/auth.js
@@ -11,24 +11,4 @@ router.route('/users').get(async (req, res) => {
   }
 });
 
-// Register
-router.route('/register').post(async (req, res) => {
-  try {
-    console.log('/register');
-    return '/register Route';
-  } catch (error) {
-    console.error(error);
-  }
-});
-
-// Login
-router.route('/login').post(async (req, res) => {
-  try {
-    console.log('/login');
-    return '/login Route';
-  } catch (error) {
-    console.error(error);
-  }
-});
-
 module.exports = router;
