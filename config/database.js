01a11cb176b3e535ac9ef4a90f48e481219e7ef8
Darwin Smith <darwin911@gmail.com>
2020-12-12 13:49:38 -0500

    adds user schema

--- config/database.js
+++ config/database.js
@@ -24,5 +24,15 @@ connection.once('open', () => {
   console.log('*** MongoDB database connection established successfully ***');
 });
 
+// Creates simple schema for a User.  The hash and salt are derived from the user's given password when they register
+const UserSchema = new mongoose.Schema({
+  username: String,
+  hash: String,
+  salt: String,
+  admin: Boolean,
+});
+
+const User = connection.model('User', UserSchema);
+
 // Expose the connection
 module.exports = connection;
