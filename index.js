6b60e487be995d77c4af8fb9e1fbba1e328e4a2f
Darwin Smith <darwin911@gmail.com>
2020-12-12 08:39:22 -0500

    updates setup with MongoStore and sessions

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const session = require('express-session');
 const passport = require('passport');
-const cors = require('cors');
+const crypto = require('crypto');
 let routes = require('./routes');
+const connection = require('./config/database');
+const cors = require('cors');
+
+const MongoStore = require('connect-mongo')(session);
 
 // Gives us access to variables set in the .env file via `process.env.VARIABLE_NAME` syntax
 require('dotenv').config();
@@ -14,8 +17,14 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 require('./config/passport');
+
+const sessionStore = new MongoStore({
+  mongooseConnection: connection,
+  collection: 'sessions',
+});
 
 /**
  * -------------- PASSPORT AUTHENTICATION ----------------
@@ -23,6 +32,18 @@ require('./config/passport');
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: sessionStore,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24, // Equals 1 day
+    },
+  })
+);
 
 app.use((req, res, next) => {
   console.log(req.session);
