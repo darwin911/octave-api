419d11541130a1624a0de898e60e31ec854ba419
Darwin Smith <darwin911@gmail.com>
2020-12-25 13:14:54 -0500

    adds get single event

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const connection = require('../config/database');
 const utils = require('../lib/utils');
 const moment = require('moment');
 const axios = require('axios');
+const API_KEY = process.env.TICKETMASTER_API_KEY;
 // const isAuth = require('./authMiddleware').isAuth;
 const User = connection.models.User;
 
@@ -176,7 +177,6 @@ router.get('/login-failure', (req, res, next) => {
 // Show all events
 router.get('/events', async (req, res, next) => {
   const dmaId = req.body.dmaId || '345';
-  const API_KEY = process.env.TICKETMASTER_API_KEY;
   let now = moment();
   const time = now.add(3, 'months');
   const threeMonthsFromNow = moment(time).format('YYYY-MM-DD');
@@ -186,6 +186,17 @@ router.get('/events', async (req, res, next) => {
       `https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=${dmaId}&endDateTime=${threeMonthsFromNow}T00:00:00Z&size=70&apikey=${API_KEY}`
     );
     return res.send(resp.data._embedded.events);
+  } catch (error) {
+    return error;
+  }
+});
+
+router.get('/events/:eventId', async (req, res, next) => {
+  try {
+    const resp = await axios.get(
+      `https://app.ticketmaster.com/discovery/v2/events/${req.params.eventId}.json?apikey=${API_KEY}`
+    );
+    return res.send(resp.data);
   } catch (error) {
     return error;
   }
