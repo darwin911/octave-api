10f964b35581b16a2e2c00736e7ded6758d442c8
Darwin Smith <darwin911@gmail.com>
2020-12-27 19:18:13 -0500

    updates routes for venues/reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 const connection = require('../config/database');
+const isEmpty = require('lodash');
 const Review = connection.models.Review;
 
 router.get('/', async (req, res, next) => {
   try {
     const reviews = await Review.find();
-    return res.send(reviews);
+    console.log(`Found ${reviews.length} reviews.`);
+    return res.send({ reviews });
   } catch (error) {
     console.error(error);
     return error;
@@ -21,7 +23,6 @@ router.get('/:id', async (req, res, next) => {
       return res.json({
         success: true,
         review,
-        retrieved: new Date().toISOString(),
       });
     }
   } catch (error) {
@@ -30,30 +31,63 @@ router.get('/:id', async (req, res, next) => {
   }
 });
 
-router.post('/', async (req, res, next) => {
+// Get All Reviews for Venue by VenueId
+router.get('/venues/:venueId', async (req, res, next) => {
   try {
-    const review = await Review.findOne({
-      where: { reviewId: req.body.reviewId },
+    console.log('params', req.params);
+    const reviews = await Review.find({ venueId: req.params.venueId });
+    let count = reviews.length;
+    console.log({ count: count });
+    if (!count) {
+      console.log('No reviews found.', `!count: ${!count}`);
+      return res.status(404).json({
+        sucess: false,
+        msg: 'No reviews found.',
+      });
+    } else {
+      console.log(`${count} reviews found.`);
+      return res.json({
+        success: true,
+        reviews,
+        count: reviews.length,
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+});
+
+router.post('/venues', async (req, res, next) => {
+  const { userId, venueId } = req.body;
+  try {
+    const venueReview = await Review.findOne({
+      venueId: venueId,
+      userId: userId,
     });
-    console.log('review: ', review);
-    if (!review) {
+
+    if (!venueReview) {
+      console.log('did not find any reviews in database for venue/user');
       // create review
       const reviewData = {
-        userId: req.body.userId,
-        venueId: req.body.venueId,
+        userId: userId,
+        venueId: venueId,
         score: Number(req.body.score),
         text: req.body.text,
       };
 
       const newReview = new Review(reviewData);
-      console.log(newReview);
       await newReview.save();
+      console.log(newReview, '\nReview created!\n');
 
       return res.status(201).json({ success: true, review: newReview });
     } else {
-      return res
-        .status(409)
-        .json({ success: false, msg: 'review already exists.' });
+      console.log('We found this review, so you will get a 409', venueReview);
+      return res.status(409).json({
+        success: false,
+        msg: 'This venue has already been reviewed by this user.',
+        previousReview: venueReview,
+      });
     }
   } catch (error) {
     res.status(500).send(error);
--- routes/venues.js
+++ routes/venues.js
@@ -2,6 +2,10 @@ const router = require('express').Router();
 const connection = require('../config/database');
 const Venue = connection.models.Venue;
 
+/**
+ * Get All Venues
+ */
+
 router.get('/', async (req, res, next) => {
   try {
     const venues = await Venue.find();
@@ -12,17 +16,35 @@ router.get('/', async (req, res, next) => {
   }
 });
 
+/**
+ * Get Venue by VenueId
+ */
+
+router.get('/:venueId', async (req, res, next) => {
+  try {
+    const venue = await Venue.findOne({ venueId: req.params.venueId });
+    if (venue) {
+      return res.send(venue);
+    } else {
+      return res.status(404).json({ msg: 'Venue not found in database.' });
+    }
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+});
+
 router.post('/', async (req, res, next) => {
   try {
-    const venue = await Venue.findOne({
+    const dbVenue = await Venue.findOne({
       where: { venueId: req.body.venueId },
     });
-    console.log('VENUE: ', venue);
-    if (!venue) {
+    console.log('VENUE: ', dbVenue);
+    if (!dbVenue) {
       // create venue
       const venueData = {
         venueId: req.body.venueId,
-        name: req.body.venueName,
+        name: req.body.name,
         url: req.body.url,
         city: req.body.city,
         state: req.body.state,
@@ -32,6 +54,8 @@ router.post('/', async (req, res, next) => {
 
       const newVenue = new Venue(venueData);
       await newVenue.save();
+
+      console.info({ success: true, venue: newVenue });
 
       return res.status(201).json({ success: true, venue: newVenue });
     } else {
