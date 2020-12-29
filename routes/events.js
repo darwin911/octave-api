b0ee42f721b32a7a83571421f6eb342924a4f276
Darwin Smith <darwin911@gmail.com>
2020-12-29 01:02:16 -0500

    refactor: pulls additional events

--- routes/events.js
+++ routes/events.js
@@ -7,30 +7,47 @@ const API_KEY = process.env.TICKETMASTER_API_KEY;
 // Show all events
 
 router.get('/:dmaId', async (req, res, next) => {
+  console.time('getEvents');
+
   try {
     const dmaId = req.params.dmaId || '345'; // Defaults to 345: New York
     let now = moment();
     const time = now.add(3, 'months');
     const threeMonthsFromNow = moment(time).format('YYYY-MM-DD');
 
-    const TM_BASE_URL = 'https://app.ticketmaster.com/discovery/v2';
+    const BASE_URL = 'https://app.ticketmaster.com';
+    const classification = 'classificationName=music';
+    const sizeVal = '200';
+    const URL = `${BASE_URL}/discovery/v2/events.json?${classification}&size=${sizeVal}&dmaId=${dmaId}&endDateTime=${threeMonthsFromNow}T00:00:00Z&apikey=${API_KEY}`;
 
-    const resp = await axios.get(
-      `${TM_BASE_URL}/events.json?classificationName=music&dmaId=${dmaId}&endDateTime=${threeMonthsFromNow}T00:00:00Z&apikey=${API_KEY}`
-    );
+    const resp = await axios.get(URL);
 
-    // console.log(resp.data._embedded.events.map((ev) => ev.dates.status.code));
-    // let activeEvents = resp.data_embedded.events.filter(
-    //   (ev) => ev.dates.code !== 'cancelled'
-    // );
-    // console.log(activeEvents);
-    // let filtedEvents = resp.data._embedded.events.filter(
-    //   (event) => event.dates.status.code !== 'cancelled'
-    // );
     const events = resp.data._embedded.events;
     console.info(`Found ${events.length} events.`);
+    const {
+      page: { size, totalElements },
+      _links,
+    } = resp.data;
+    console.log({ size, totalElements });
+    if (size < totalElements) {
+      const nextURL = `${BASE_URL}${_links.next.href}&apikey=${API_KEY}`;
+      console.log('fetching more events...');
+      console.log({ nextURL });
+      const nextData = await axios.get(nextURL);
+
+      const nextEvents = nextData.data._embedded.events;
+      console.log(`\nNext Events:\n`, nextEvents.length, '\n');
+
+      const allEvents = [...events, ...nextEvents];
+
+      console.log(`Found ${allEvents.length} in All events.`);
+      console.timeEnd('getEvents');
+      return res.send(allEvents);
+    }
+    console.timeEnd('getEvents');
     return res.send(events);
   } catch (error) {
+    console.log(error);
     return error;
   }
 });
